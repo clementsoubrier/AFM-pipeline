107cf6f5f6dc372964db3d2494b0d2edc848b3f3
Eshan Nirody <nirody@zoology.ubc.ca>
2023-05-17 05:31:14 +0000

    wrap in get_individuals function

--- extract_individuals.py
+++ extract_individuals.py
@@ -21,6 +21,27 @@ adj_mat = np.load(dictionary_dir + "Bool_matrix.npy") #asymmetric adjacency matr
 
 #%% Adjacency matrix to dictionary functions
 
+def extract_individuals(adj_mat,save_dir,filename='ROI_dict'):
+    """
+    Wrapper function. Accepts an adjacency matrix as its input, extracts all 
+    individual cell information, and returns and saves a dictionary of ROIs to 
+    the specified save directory.
+    """
+    ROI_dict = {} # initialize dictionary
+    roots = get_roots(adj_mat) # get all roots and start new tree traversal for each one 
+    starting_num = 1 # initial ROI ID number is 1
+    
+    for root in roots:
+        individuals = get_ROIs(adj_mat,root,ROI_num=starting_num,root=root)[0]
+        ROI_dict.update(list2dict(individuals))
+        starting_num = int(list(ROI_dict.keys())[-1].split()[-1])+1 # make sure next tree's first ROI is latest ROI ID# + 1
+    
+    create_children(ROI_dict) # add Children key to each dictionary item
+    np.save(save_dir+filename,ROI_dict)
+    # print(ROI_dict)
+    return ROI_dict
+    
+
 def get_roots(adj_mat):
     """ 
     Accepts an adjacency matrix as its input and returns a list of roots of 
@@ -30,9 +51,10 @@ def get_roots(adj_mat):
     roots = list(np.where(roots)[0])
     return roots
 
-def get_individuals(adj_mat,current_node,ROI_num=1,root=0):
+
+def get_ROIs(adj_mat,current_node,ROI_num=1,root=0):
     """
-    Performs a depth first traversal of the binary tree represented by the 
+    Performs a depth first traversal of the binary tree represented in the 
     adjacency matrix input argument and returns 1) a flat list of strings and 
     integers corresponding to each ROI ID, the parent ROI ID, and the mask 
     indices (nodes) for each ROI; 2) the number ID of the last individual ROI 
@@ -50,7 +72,7 @@ def get_individuals(adj_mat,current_node,ROI_num=1,root=0):
         # recursively call the function
         # get a list of all successor nodes for the current node
         # keep track of the ID number of the newest ROI 
-        successors, ROI_num = get_individuals(adj_mat,next_node,ROI_num=ROI_num)
+        successors, ROI_num = get_ROIs(adj_mat,next_node,ROI_num=ROI_num)
         individuals.extend(successors)
         
     # if there are multiple child nodes, then a division event has occurred    
@@ -63,9 +85,10 @@ def get_individuals(adj_mat,current_node,ROI_num=1,root=0):
             individuals.append('Parent: ROI '+str(parent_num))
             # recursively get all successor nodes for this new individual
             # keep track of newest ROI ID number
-            successors, ROI_num = get_individuals(adj_mat,next_node,ROI_num=ROI_num) 
+            successors, ROI_num = get_ROIs(adj_mat,next_node,ROI_num=ROI_num) 
             individuals.extend(successors)       
     return individuals, ROI_num
+
 
 def list2dict(individuals):
     """
@@ -91,6 +114,7 @@ def list2dict(individuals):
             ROI_dict[ROI]['Mask IDs'].append(element)
     return ROI_dict
 
+
 def create_children(ROI_dic):
     """
     Add "Children" key to complement "Parent" key for each ROI. Append empty
@@ -102,27 +126,9 @@ def create_children(ROI_dic):
         if ROI_dic[ROI]['Parent']!='':
           ROI_dic[ROI_dic[ROI]['Parent']]['Children'].append(ROI)
 
+#%% Obtain full ROI dictionary from the adjacency matrix and save to specified directory
 
-#%% Obtain ROI dictionary from the adjacency matrix
-
-ROI_dict = {} # initialize dictionary
-
-roots = get_roots(adj_mat) # get all roots and start new tree traversal for each one 
-starting_num = 1 # initial ROI ID number is 1
-
-for root in roots:
-    individuals = get_individuals(adj_mat,root,ROI_num=starting_num,root=root)[0]
-    ROI_dict.update(list2dict(individuals))
-    starting_num = int(list(ROI_dict.keys())[-1].split()[-1])+1 # make sure next tree's first ROI is latest ROI ID# + 1
-
-create_children(ROI_dict) # add Children key to each dictionary item
-
-print(ROI_dict)
-
-#%% Save the output ROI dictionary
-
-dict_filename = 'ROI_dict.npy' 
-np.save(dictionary_dir + dict_filename,ROI_dict)
+extract_individuals(adj_mat,dictionary_dir,filename='ROI_dict')
 
 
 #%% Load and check ROI dictionary
